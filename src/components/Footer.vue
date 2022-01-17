f32887a0a8dc2afff8cfaf00971a19a756712153
silence <beast964089@gmail.com>
2022-01-17 16:33:01 +0800

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>footer</div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+export default {
+  name: "Footer",
+  setup() {
+    onMounted(() => {
+      console.log("mounted");
+    });
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="positon-fixed d-flex p-4">
+    <div>Navbar</div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+nav {
+  width: 100%;
+}
+</style>
